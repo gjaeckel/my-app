58b129b38f9da9783423e6c9cf3ecf6ca28d5660
gjaeckel <eternal1@eternalpro.com>
2021-07-27 11:36:21 -0500

    tutorial changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function App1() {
+
+  const [status, setStatus] = useState("Open");
+  return (
+    <>
+      <h1>
+        Status {status}
+      </h1>
+      <button onClick={() => setStatus("Open")}>Open</button>
+      <button onClick={() => setStatus("Back in 5")}>Break</button>
+      <button onClick={() => setStatus("Closed")}>Closed</button>
+    </>
+  );
+}
+
+function Checkbox() {
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false
+  );
+  // useEffect(() => {
+  //   alert(`checked: ${checked.toString()}`);
+  // });
+  return (
+    <>
+      <input type="checkbox" value={checked} onChange={toggle} />
+      {checked ? "checked" : "not checked"}
+    </>
+  );
+
+}
+
+function GitHubUser({ login }) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.error)
+  }, []);
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.login}</h1>
+      </div>
+    )
+  }
+  return null;
+}
+
+function App3() {
+  return <GitHubUser login="gjaeckel" />;
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Checkbox />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
